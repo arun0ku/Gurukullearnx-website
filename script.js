2dd49bf17182907ebc4d9cd6a47cb12fbea0be86
arun0ku <144062400+arun0ku@users.noreply.github.com>
2026-01-23 00:10:27 +0530

    Implement page loader visibility on load event
    Add page loader functionality on window load.

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // Future enhancements (analytics, animations)
 console.log("Gurukul LearnX website loaded");
+
+window.addEventListener("load", function(){
+  const loader = document.getElementById("page-loader");
+  loader.style.opacity = "0";
+  setTimeout(()=>{
+    loader.style.display = "none";
+  }, 600);
+});
